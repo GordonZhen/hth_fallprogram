b4cf3563a5ee19936512ac419a09d3ec667524f1
Gordon Zhen <gordonzhen1@gmail.com>
2022-10-06 19:55:50 -0700

    about me python 1

---
+++ Getting-Started-with-Python-gordonzhen/about_me.py
@@ -0,0 +1,13 @@
+first_last = "Gordon Zhen"
+
+prefer_name = "Gordon"
+
+age = 17
+
+used_python = True
+
+fav_things = ['watching anime','working out','spending time with friends']
+
+fav_things_dict = {'watching anime':'I like the genres Fantasy , Isekai , Romance , Sports , Action','working out':'going to the gym with my brother following his routine','spending time with friends':'playing video games with them and hang out with them during school'}
+
+all_vars = dict(vars())
